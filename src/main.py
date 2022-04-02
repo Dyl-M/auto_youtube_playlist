b80f1f39e4b284b7f64f5a211d232b457f2f7bf1
Dylan Monfret <dylmpro21@gmail.com>
2022-04-02 14:21:38 +0200

    refactor(r/src): Comments addition for a better understanding of the code + minor changes.

--- src/main.py
+++ src/main.py
@@ -21,12 +21,16 @@ with open('../data/music_channels.json', 'r', encoding='utf8') as music_channel_
 with open('../data/playlists.json', 'r', encoding='utf8') as playlists_file:
     PLAYLISTS = json.load(playlists_file)
 
-PLAYLIST_MIXES_ID, PLAYLIST_LIVES_ID = PLAYLISTS['mixes']['id'], PLAYLISTS['lives']['id']
+PLAYLIST_MIXES, PLAYLIST_LIVES = PLAYLISTS['mixes']['id'], PLAYLISTS['lives']['id']
 MUSIC_CHANNELS_IDS = [channel['id'] for channel in MUSIC_CHANNELS]
 MUSIC_CHANNELS_UPLOADS = [channel['uploads'] for channel in MUSIC_CHANNELS]
 
-"MAIN"
+"MAIN"  # TODO: implement logging
 
-if __name__ == '__main__':
-    LAST_DAY = youtube_req.iter_playlists(playlist_list=MUSIC_CHANNELS_UPLOADS, service=YOUTUBE_OAUTH, day_ago=1)
-    youtube_req.update_playlist(service=YOUTUBE_OAUTH, playlist_id=PLAYLIST_MIXES_ID, videos_to_add=LAST_DAY)
+# Update live playlist
+CURRENT_LIVE = youtube_req.iter_livestreams(channel_list=MUSIC_CHANNELS_IDS)
+youtube_req.update_playlist(service=YOUTUBE_OAUTH, playlist_id=PLAYLIST_LIVES, videos_to_add=CURRENT_LIVE, is_live=True)
+
+# Update mixes playlist
+LAST_DAY = youtube_req.iter_playlists(playlist_list=MUSIC_CHANNELS_UPLOADS, service=YOUTUBE_OAUTH, day_ago=1)
+youtube_req.update_playlist(service=YOUTUBE_OAUTH, playlist_id=PLAYLIST_MIXES, videos_to_add=LAST_DAY)
--- src/youtube_req.py
+++ src/youtube_req.py
@@ -32,24 +32,24 @@ pd.set_option('display.width', 250)
 
 "GLOBAL"
 
-TODAY = dt.datetime.now(tz=pytz.utc).replace(hour=0, minute=0, second=0, microsecond=0)
+TODAY = dt.datetime.now()
 
 "FUNCTIONS"
 
 
-def get_authenticated_service():  # TODO: Comment
+def get_authenticated_service():
     """Retrieve authentification credentials at specified path or create new ones
     :return: a Google API service object build with 'googleapiclient.discovery.build'.
     """
-    oauth_file = '../tokens/oauth.json'
+    oauth_file = '../tokens/oauth.json'  # OAUTH 2.0 ID path
     scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
     credential_path = os.path.join('../tokens/', 'credential.json')
-    store = Storage(credential_path)
-    credentials = store.get()
+    store = Storage(credential_path)  # Store and retrieve a single credential to and from a file
+    credentials = store.get()  # Retrieve credential
 
-    if not credentials or credentials.invalid:
-        flow = oauth2client_client.flow_from_clientsecrets(oauth_file, scopes)
-        credentials = oauth2client_tools.run_flow(flow, store)
+    if not credentials or credentials.invalid:  # Cover lack of credentials and outdated credentials
+        flow = oauth2client_client.flow_from_clientsecrets(oauth_file, scopes)  # Create a Flow from 'oauth_file'
+        credentials = oauth2client_tools.run_flow(flow, store)  # Run authentification process
 
     return googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
 
@@ -92,7 +92,7 @@ def get_channels(service: googleapiclient.discovery, channel_list: list, save: b
 
 
 def get_playlist_items(service: googleapiclient.discovery, playlist_id: str, day_ago: int = None,
-                       ref_date: dt.datetime = TODAY):  # TODO: Comment
+                       ref_date: dt.datetime = TODAY):
     """Get the videos in a YouTube playlist
     :param service: a YouTube service build with 'googleapiclient.discovery'
     :param playlist_id: a YouTube playlist ID
@@ -106,21 +106,32 @@ def get_playlist_items(service: googleapiclient.discovery, playlist_id: str, day
 
     while 1:
         try:
-            request = service.playlistItems().list(part=['snippet', 'contentDetails', 'status'], playlistId=playlist_id,
-                                                   maxResults=50, pageToken=next_page_token).execute()
+            request = service.playlistItems().list(part=['snippet', 'contentDetails', 'status'],
+                                                   playlistId=playlist_id,
+                                                   maxResults=50,
+                                                   pageToken=next_page_token).execute()  # Request playlist's items
 
             p_items += [{'video_id': item['contentDetails']['videoId'],
-                         'upload_date': dt.datetime.strptime(item['contentDetails']['videoPublishedAt'], date_format),
+                         'release_date': dt.datetime.strptime(item['contentDetails']['videoPublishedAt'], date_format),
                          'channel_id': item['snippet']['videoOwnerChannelId'],
                          'channel_name': item['snippet']['videoOwnerChannelTitle'],
                          'item_id': item['id'],
-                         'status': item['status']['privacyStatus']} for item in request['items']]
+                         'status': item['status']['privacyStatus']} for item in request['items']]  # Keep necessary data
 
-            if day_ago is not None:
+            if day_ago is not None:  # In case we want to keep videos published x days ago from your ref_date
+                # Hours of reference date set to midnight UTC
+                ref_date = ref_date.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=pytz.utc)
+
+                # Days subtraction
                 date_delta = ref_date - dt.timedelta(days=day_ago)
-                p_items = [item for item in p_items if ref_date > item['upload_date'] > date_delta]
 
-                if len(p_items) <= 50:
+                # Hours of reference date reset to 23:59:59 UTC (to keep videos released during the day)
+                ref_date = ref_date.replace(hour=23, minute=59, second=59)
+
+                # Filtering
+                p_items = [item for item in p_items if ref_date > item['release_date'] > date_delta]
+
+                if len(p_items) <= 50:  # No need for more requests (the playlist must be ordered chronologically!)
                     break
 
             next_page_token = request.get('nextPageToken')
@@ -132,9 +143,9 @@ def get_playlist_items(service: googleapiclient.discovery, playlist_id: str, day
             error_reason = http_error.error_details[0]['reason']
 
             if error_reason == 'playlistNotFound':
-                break  # TODO: Define what to do if there is no public video.
+                break  # TODO: Define what to do if there is no public video (logging).
 
-            print(f"Unknown error\n{http_error}")  # TODO: Define what to do in this case.
+            print(f"Unknown error\n{http_error}")  # TODO: Define what to do in this case (logging).
             sys.exit()
 
     return p_items
@@ -149,10 +160,10 @@ def get_videos(service: googleapiclient.discovery, videos_list: list):
     return service.videos().list(part=['snippet', 'contentDetails'], id=",".join(videos_list), maxResults=50).execute()
 
 
-def find_lives(channel_id: str):
-    """Find live streams on YouTube using a channel ID
+def find_livestreams(channel_id: str):
+    """Find livestreams on YouTube using a channel ID
     :param channel_id: a YouTube channel ID
-    :return live_list: list of live stream ID (or empty list if no live at the moment).
+    :return live_list: list of livestream ID (or empty list if no livestream at the moment).
     """
     cookies = {'CONSENT': 'YES+cb.20210328-17-p0.en-GB+FX+{}'.format(random.randint(100, 999))}  # Cookies settings
     web_page = requests.get(f'https://www.youtube.com/channel/{channel_id}', cookies=cookies)  # Page request
@@ -168,18 +179,18 @@ def find_lives(channel_id: str):
     # Extract content from channel page items
     section = tab['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]
 
-    if 'channelFeaturedContentRenderer' in section.keys():  # If at least one live stream is running
-        # Extract each live stream item
+    if 'channelFeaturedContentRenderer' in section.keys():  # If at least one livestream is running
+        # Extract each livestream item
         featured = section['channelFeaturedContentRenderer']['items']
 
-        # Extract live stream IDs channel_id
-        live_list = [{'channel_id': channel_id, 'video_id': item['videoRenderer']['videoId']} for item in featured]
-        return live_list
+        # Extract livestream IDs channel_id
+        livestream_ids = [{'channel_id': channel_id, 'video_id': item['videoRenderer']['videoId']} for item in featured]
+        return livestream_ids
 
-    return []  # To return if no live stream at the moment
+    return []  # To return if no livestream at the moment
 
 
-def check_if_live(service: googleapiclient.discovery, videos_list: list):  # TODO: Comment a bit more
+def check_if_live(service: googleapiclient.discovery, videos_list: list):
     """Get broadcast status with YouTube video IDs
     :param service: a YouTube service build with 'googleapiclient.discovery'
     :param videos_list: list of YouTube video IDs
@@ -193,8 +204,10 @@ def check_if_live(service: googleapiclient.discovery, videos_list: list):  # TOD
     for chunk in videos_chunks:
         try:
             request = get_videos(service=service, videos_list=chunk)
-            items += [{'video_id': item['id'], 'live_status': item['snippet']['liveBroadcastContent']}
-                      for item in request['items']]
+
+            # Keep necessary data
+            items += [{'video_id': item['id'],
+                       'live_status': item['snippet']['liveBroadcastContent']} for item in request['items']]
 
         except googleapiclient.errors.HttpError:
             print('googleapiclient.errors.HttpError')
@@ -203,7 +216,7 @@ def check_if_live(service: googleapiclient.discovery, videos_list: list):  # TOD
     return items
 
 
-def get_durations(service: googleapiclient.discovery, videos_list: list):  # TODO: Comment a bit more
+def get_durations(service: googleapiclient.discovery, videos_list: list):
     """Get duration of YouTube video with their ID
     :param service: a YouTube service build with 'googleapiclient.discovery'
     :param videos_list: list of YouTube video IDs
@@ -218,9 +231,11 @@ def get_durations(service: googleapiclient.discovery, videos_list: list):  # TOD
     for chunk in videos_chunks:
         try:
             request = get_videos(service=service, videos_list=chunk)
+
+            # Keep necessary data
             items += [{'video_id': item['id'],
-                       'duration': isodate.parse_duration(item['contentDetails']['duration']).seconds}
-                      for item in request['items']]
+                       'duration': isodate.parse_duration(item['contentDetails']['duration']).seconds / 60,
+                       'live_status': item['snippet']['liveBroadcastContent']} for item in request['items']]
 
         except googleapiclient.errors.HttpError:
             print('googleapiclient.errors.HttpError')
@@ -229,12 +244,12 @@ def get_durations(service: googleapiclient.discovery, videos_list: list):  # TOD
     return items
 
 
-def iter_lives(channel_list: list):
-    """Apply 'find_lives' for a collection of YouTube channel
+def iter_livestreams(channel_list: list):
+    """Apply 'find_livestreams' for a collection of YouTube channel
     :param channel_list: list of YouTube channel IDs
-    :return lives_list: IDs of current live based on channels collection.
+    :return: IDs of current live based on channels collection.
     """
-    lives_it = [find_lives(chan_id) for chan_id in tqdm.tqdm(channel_list, desc='Looking for live streams')]
+    lives_it = [find_livestreams(chan_id) for chan_id in tqdm.tqdm(channel_list, desc='Looking for livestreams')]
     return list(itertools.chain.from_iterable(lives_it))
 
 
@@ -245,7 +260,7 @@ def iter_playlists(playlist_list: list, service: googleapiclient.discovery, day_
     :param service: a YouTube service build with 'googleapiclient.discovery'
     :param day_ago: day difference with a reference date, delimits items' collection field
     :param ref_date: reference date (TODAY by default)
-    :return lives_list: IDs of current live based on channels collection.
+    :return: videos retrieved in playlists.
     """
     item_it = [get_playlist_items(service=service, playlist_id=playlist_id, day_ago=day_ago, ref_date=ref_date)
                for playlist_id in tqdm.tqdm(playlist_list, desc='Looking for videos to add')]
@@ -253,57 +268,69 @@ def iter_playlists(playlist_list: list, service: googleapiclient.discovery, day_
 
 
 def update_playlist(service: googleapiclient.discovery, playlist_id: str, videos_to_add: list = None,
-                    is_live: bool = False, min_duration: int = 10, day_ago: int = 6):  # TODO: Finish this
+                    is_live: bool = False, min_duration: int = 10, del_day_ago: int = 7,
+                    ref_date: dt.datetime = TODAY):
     """Update a YouTube playlist with temporal criteria
     :param service: a YouTube service build with 'googleapiclient.discovery'
     :param playlist_id: a YouTube playlist ID
     :param videos_to_add: list of YouTube video IDs to potentially add to a specified playlist
-    :param is_live: to update a list containing specifically live steams only (or not)
-    :param day_ago: day difference with TODAY, to keep in playlist video published in the last 'day_ago' days
+    :param is_live: to update a list containing specifically livestreams only (or not)
+    :param del_day_ago: day difference with TODAY, to keep in playlist video published in the last 'del_day_ago' days
+    :param ref_date: reference date (TODAY by default)
     :param min_duration: minimal video duration filter.
     """
-    already_in = pd.DataFrame(get_playlist_items(service=service, playlist_id=playlist_id))
+    # Pass playlist as pandas Dataframes (for easier filtering)
+    already_in = pd.DataFrame(get_playlist_items(service=service, playlist_id=playlist_id))  # Get videos already in
     to_add_df = pd.DataFrame(videos_to_add)
 
-    if not already_in.empty:
-        to_add_df = to_add_df.loc[~to_add_df.video_id.isin(already_in.video_id)]
-        in_playlist_ids = already_in.video_id.to_list()
+    if not already_in.empty:  # If there is at least one video in the playlist
+        to_add_df = to_add_df.loc[~to_add_df.video_id.isin(already_in.video_id)]  # Check if new videos are already in
 
-        if is_live:
-            live_status = pd.DataFrame(check_if_live(service=service, videos_list=in_playlist_ids))
+        if is_live:  # If the update is done on a YouTube livestreams playlist
+            # Retrieve live status
+            live_status = pd.DataFrame(check_if_live(service=service, videos_list=already_in.video_id))
             already_in = already_in.merge(live_status)
+
+            # Do not keep private videos. Do not keep upcoming and finished livestreams.
             to_delete_df = already_in.loc[(already_in.status == 'private') | (already_in.live_status != 'live')]
 
-            if not to_add_df.empty:
+            if not to_add_df.empty:  # If there are videos to add
                 add_to_playlist(service=service, playlist_id=playlist_id, videos_list=to_add_df.video_id)
 
-            if not to_delete_df.empty:
+            if not to_delete_df.empty:  # If there are videos to delete
                 del_from_playlist(service=service, playlist_id=playlist_id, items_list=to_delete_df.item_id)
 
         else:
-            date_delta = TODAY - dt.timedelta(days=day_ago)
-            delete_cond = (already_in.status == 'private') | (already_in.upload_date < date_delta)
-            to_delete_df = already_in.loc[delete_cond]
+            date_delta = ref_date - dt.timedelta(days=del_day_ago)  # Days subtraction
+            delete_cond = (already_in.status == 'private') | (already_in.release_date < date_delta)  # Delete condition
+            to_delete_df = already_in.loc[delete_cond]  # To keep public and newest videos.
+
+            # Get durations of videos to add
             durations = pd.DataFrame(get_durations(service=service, videos_list=videos_to_add))
             to_add_df = to_add_df.merge(durations)
-            to_add_df = to_add_df.loc[to_add_df.duration >= min_duration * 60].reset_index(drop=True)
 
-            if not to_add_df.empty:
+            # Keep videos with duration above `min_duration` minutes and don't keep "Premiere" type videos
+            to_add_df = to_add_df.loc[(to_add_df.duration >= min_duration) & (to_add_df.live_status != 'upcoming')]
+
+            if not to_add_df.empty:  # If there are videos to add
                 add_to_playlist(service=service, playlist_id=playlist_id, videos_list=to_add_df.video_id)
 
-            if not to_delete_df.empty:
+            if not to_delete_df.empty:  # If there are videos to delete
                 del_from_playlist(service=service, playlist_id=playlist_id, items_list=to_delete_df.item_id)
 
-    else:
-        if is_live and to_add_df.empty:
+    else:  # If there is no video in the playlist
+        if is_live and to_add_df.empty:  # If there are livestreams to add
             add_to_playlist(service=service, playlist_id=playlist_id, videos_list=to_add_df.video_id)
 
-        else:
+        else:  # For regular videos
+            # Get durations of videos to add
             durations = pd.DataFrame(get_durations(service=service, videos_list=videos_to_add))
             to_add_df = to_add_df.merge(durations)
-            to_add_df = to_add_df.loc[to_add_df.duration >= min_duration * 60].reset_index(drop=True)
 
-            if not to_add_df.empty:
+            # Keep videos with duration above `min_duration` minutes and don't keep "Premiere" type videos
+            to_add_df = to_add_df.loc[(to_add_df.duration >= min_duration) & (to_add_df.live_status != 'upcoming')]
+
+            if not to_add_df.empty:  # If there are videos to add
                 add_to_playlist(service=service, playlist_id=playlist_id, videos_list=to_add_df.video_id)
 
 
