62c812328d823b284d07d57837ac16b60f20fa4e
Dylan Monfret <dylmpro21@gmail.com>
2022-04-08 13:17:18 +0200

    fix(r/src): PTC-W6004.
    Fix "External control of file name or path" (reviewed with DeepSource).

--- src/youtube_req.py
+++ src/youtube_req.py
@@ -36,12 +36,11 @@ pd.set_option('display.width', 250)
 "GLOBAL"
 
 
-def last_exe_date(logging_path: str = '../log/last_exe.log'):
+def last_exe_date():
     """Extract last execution datetime from a log file (supposing first line is containing the right datetime).
-    :param logging_path: .log file path
     :return date: last execution date.
     """
-    with open(logging_path, 'r', encoding='utf8') as log_file:
+    with open('../log/last_exe.log', 'r', encoding='utf8') as log_file:
         first_log = log_file.readlines()[0]  # Get first log
 
     date_str = re.search(r'(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})\+(\d{4})', first_log).group()  # Extract date
