0333d011bad07b97d5089e19ab142ac039bfe545
dyl_m <dylmpro21@gmail.com>
2022-04-23 18:19:20 +0200

    ci(r/src): testing python file execution with GCP authentication.

--- src/_test.py
+++ src/_test.py
@@ -1,10 +1,35 @@
 # -*- coding: utf-8 -*-
 
+import googleapiclient.discovery
+import glob
+import sys
+
+from google.oauth2.credentials import Credentials
+
 """File Information
 @file_name: _test.py
 @author: Dylan "dyl-m" Monfret
 To test things / backup functions.
 """
 
+
+def create_service():
+    """"
+    :return:
+    """
+    cred_file = glob.glob("../gha-creds-*.json")[0]
+    cred = Credentials.from_authorized_user_file(cred_file)  # Retrieve credentials
+
+    try:
+        service = googleapiclient.discovery.build('youtube', 'v3', credentials=cred)
+        print('YouTube Service created successfully.')
+        return service
+
+    except Exception as error:  # skipcq: PYL-W0703 - No known errors at the moment.
+        print(error)
+        print('Failed to create service instance for YouTube')
+        sys.exit()
+
+
 if __name__ == '__main__':
-    print("Hello world :)")
+    YOUTUBE = create_service()
