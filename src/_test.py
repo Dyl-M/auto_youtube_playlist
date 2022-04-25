c413c5f1d56d6816e6b27cd093edf7643ee4f678
dyl_m <dylmpro21@gmail.com>
2022-04-25 18:38:43 +0200

    ci(r/src): testing command line arguments.

--- src/_test.py
+++ src/_test.py
@@ -1,26 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import google.auth
-import googleapiclient.discovery
-import glob
-
-from google.auth import impersonated_credentials
-from google.oauth2 import service_account
+import sys
 
 """File Information
 @file_name: _test.py
 @author: Dylan "dyl-m" Monfret
-To test things / backup functions: currently testing Google Cloud authentication.
+To test things / backup functions.
 """
 
-
-def create_service():
-    """Retrieve authentication credentials"""
-    scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-    credentials, _ = google.auth.default(scopes=scopes)
-    service = googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
-    return service
-
+try:
+    exe_mode = sys.argv[1]
+except IndexError:
+    exe_mode = 'local'
 
 if __name__ == '__main__':
-    print(create_service())
+    print('Hello world :)')
+    print(f'Executing script {sys.argv[0]} in {exe_mode} mode.')
