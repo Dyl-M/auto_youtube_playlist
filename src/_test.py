376e6c9e19d2ba22e37f81eef2cec4c93f23c96f
dyl_m <dylmpro21@gmail.com>
2022-04-25 16:20:08 +0200

    ci(r/src): testing GCP authentication from .py script (step 2 - try 8).

--- src/_test.py
+++ src/_test.py
@@ -16,7 +16,14 @@ def create_service():
     """Retrieve authentication credentials"""
     service_account_file = glob.glob("../gha-creds-*.json")[0]
     scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-    credentials = service_account.IDTokenCredentials.from_service_account_file(service_account_file, scopes=scopes)
+    token_uri = 'https://oauth2.googleapis.com/token'
+    client_email = 'github-x-google@youtube-automatic-playlists.iam.gserviceaccount.com'
+
+    credentials = service_account.IDTokenCredentials.from_service_account_file(service_account_file,
+                                                                               scopes=scopes,
+                                                                               client_email=client_email,
+                                                                               token_uri=token_uri)
+
     service = googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
     return service
 
