4b7eeed58068d2d0667222c72e1d36ee740ac266
dyl_m <dylmpro21@gmail.com>
2022-04-25 18:49:41 +0200

    chore(r/src): _test.py file cleaned.

--- src/_test.py
+++ src/_test.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
 """File Information
 @file_name: _test.py
 @author: Dylan "dyl-m" Monfret
 To test things / backup functions.
 """
 
-try:
-    exe_mode = sys.argv[1]
-except IndexError:
-    exe_mode = 'local'
-
 if __name__ == '__main__':
     print('Hello world :)')
-    print(f'Executing script {sys.argv[0]} in {exe_mode} mode.')
