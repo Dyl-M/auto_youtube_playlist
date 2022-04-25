70615a1677402d6f975c7d38e988fc826f80d7b9
dyl_m <dylmpro21@gmail.com>
2022-04-25 16:16:26 +0200

    ci(r/src): testing GCP authentication from .py script (step 2 - try 7).

--- src/_test.py
+++ src/_test.py
@@ -14,16 +14,9 @@ To test things / backup functions: currently testing Google Cloud authentication
 
 def create_service():
     """Retrieve authentication credentials"""
-    import json
     service_account_file = glob.glob("../gha-creds-*.json")[0]
-
-    with open(service_account_file, 'r', encoding='utf8') as service_account_json:
-        json_file = json.load(service_account_json)
-
-    print(json_file.keys())
-
     scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-    credentials = service_account.Credentials.from_service_account_file(service_account_file, scopes=scopes)
+    credentials = service_account.IDTokenCredentials.from_service_account_file(service_account_file, scopes=scopes)
     service = googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
     return service
 
