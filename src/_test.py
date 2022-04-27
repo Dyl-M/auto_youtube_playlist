73a9b0c1890b12d93504fa07aeec37289c760b88
dyl_m <dylmpro21@gmail.com>
2022-04-27 12:21:11 +0200

    ci(r/src): test OAUTH secret.

--- src/_test.py
+++ src/_test.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
 """File Information
 @file_name: _test.py
 @author: Dylan "dyl-m" Monfret
-To test things / backup functions.
+To test things / backup functions. Currently, testing authentication from GitHub Repo Secrets.
 """
+
+oauth = sys.argv[1]
 
 if __name__ == '__main__':
     print('Hello world :)')
+    print(oauth[:100])
