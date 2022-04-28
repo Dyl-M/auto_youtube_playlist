b8047a0674a20250688c65afe4f0685e28e78f6d
dyl_m <dylmpro21@gmail.com>
2022-04-28 15:23:05 +0200

    chore(r/src): cleaning _test.py.

--- src/_test.py
+++ src/_test.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import ast
-import base64
-import sys
-
 """File Information
 @file_name: _test.py
 @author: Dylan "dyl-m" Monfret
-To test things / backup functions. Currently, testing authentication from GitHub Repo Secrets.
+To test things / backup functions.
 """
-
-oauth_str = sys.argv[1]
 
 if __name__ == '__main__':
     print('Hello world :)')
-    oauth = ast.literal_eval(base64.urlsafe_b64decode(oauth_str).decode('utf-8'))
-    print(oauth['web'].keys())
