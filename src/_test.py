16cf60fa37453565d588ba07fa17b9b1421768db
dyl_m <dylmpro21@gmail.com>
2022-05-13 17:56:16 +0200

    ci(r/src): testing authentication and credentials update within Python script.

--- src/_test.py
+++ src/_test.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import ast
+import base64
+import googleapiclient.discovery
+import googleapiclient.errors
+import json
 import os
+import sys
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
 """File Information
 @file_name: _test.py
@@ -13,9 +22,45 @@ def create_service_test():
     """Retrieve authentication credentials from dedicated repository secrets
     :return service: a Google API service object build with 'googleapiclient.discovery.build'.
     """
-    oauth_b64 = os.environ.get('OAUTH_B64')
-    print(oauth_b64[:10])
+
+    def import_env_var(var_name: str):
+        """Import variable environment and perform base64 decoding
+        :param var_name: environment variable name
+        :return value: decoded value
+        """
+        v_b64 = os.environ.get(var_name)  # Get environment variable
+        v_str = base64.urlsafe_b64decode(v_b64).decode(encoding='utf8')  # Decode
+        value = ast.literal_eval(v_str)  # Eval
+        return value
+
+    creds_dict = import_env_var(var_name='CREDS_B64')  # Import pre-registered credentials
+    creds = Credentials.from_authorized_user_info(creds_dict)  # Conversion to suitable object
+    instance_fail_message = 'Failed to create service instance for YouTube'
+
+    if not creds.valid:  # Cover outdated credentials
+        if creds.expired and creds.refresh_token:
+            creds.refresh(Request())  # Refresh token
+
+            # Get refreshed token as JSON-like string
+            creds_str = json.dumps(ast.literal_eval(creds.to_json())).encode('utf-8')
+
+            creds_b64 = str(base64.urlsafe_b64encode(creds_str))[2:-1]  # Encode token
+            os.environ["CREDS_B64"] = creds_b64  # Update environment variable value
+            print('API credentials refreshed.')
+
+        else:
+            print('ERROR: Unable to refresh credentials. Check Google API OAUTH parameter.')
+            sys.exit()
+
+    try:
+        service = googleapiclient.discovery.build('youtube', 'v3', credentials=creds)  # Build service.
+        print('YouTube service created successfully.')
+        return service
+
+    except Exception as error:  # skipcq: PYL-W0703 - No known errors at the moment.
+        print(f'({error}) {instance_fail_message}')
+        sys.exit()
 
 
 if __name__ == '__main__':
-    create_service_test()
+    SERVICE = create_service_test()
