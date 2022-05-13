d71fd6f2821ffddfc2d66059e72ce989f2ab52ad
dyl_m <dylmpro21@gmail.com>
2022-05-13 16:19:49 +0200

    ci(r/src): testing environment variable usage in Python file.

--- src/_test.py
+++ src/_test.py
@@ -5,8 +5,17 @@ import os
 """File Information
 @file_name: _test.py
 @author: Dylan "dyl-m" Monfret
-To test things / backup functions.
+To test things / backup functions. Testing authentication.
 """
+
+
+def create_service_test():
+    """Retrieve authentication credentials from dedicated repository secrets
+    :return service: a Google API service object build with 'googleapiclient.discovery.build'.
+    """
+    oauth_b64 = os.environ.get('OAUTH_B64')
+    print(oauth_b64[:10])
+
 
 if __name__ == '__main__':
     create_service_test()
